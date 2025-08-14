f2c9dee2d68d14ab7c9226c1a61f828fcb63bdc1
hanseungwan1 <115906460+hanseungwan1@users.noreply.github.com>
2025-08-14 16:03:12 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 304 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){   
+    int a = 0;
+    scanf("%d", &a);
+    
+    for(int i = 0; i < a; i++){
+        int b = 0, c = 0;
+        scanf("%d %d", &b, &c);
+        printf("%d\n", b + c);
+    }
+
+    
+}
