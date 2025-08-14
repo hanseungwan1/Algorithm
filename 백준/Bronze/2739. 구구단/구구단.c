68ae0a50bab6c13a47b6d5b728801e9a6dde563a
hanseungwan1 <115906460+hanseungwan1@users.noreply.github.com>
2025-08-14 14:11:24 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.c
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    int input = 0;
-    scanf("%d", &input);
-    int max = 10;
-    for(int i = 1; i < max; i++) {
-        int result = input * i;
-
-        printf("%d * %d = %d", input, i, result);
-        if (i < max - 1) {
-            printf("\n");
-        } 
+int main(void){
+    
+    int a = 0;
+    scanf("%d", &a);
+    
+    for(int i = 1; i < 10; i++){
+        printf("%d * %d = %d\n",a, i, a * i);
     }
     
 }
